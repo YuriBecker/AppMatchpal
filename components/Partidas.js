0799e18bd1f41dc13d4b42a06e70000678c0f19c
Yuri Becker <becker.yr@gmail.com>
2019-03-24 16:48:24 -0300

    Fix partidas

--- components/Partidas.js
+++ components/Partidas.js
@@ -1,12 +1,26 @@
 import React, { Component } from 'react';
-import { Text, View, AsyncStorage } from 'react-native';
+import { View, FlatList } from 'react-native';
 import store from 'react-native-simple-store';
+import { ListItem, Text } from 'react-native-elements';
+import Icon from '@expo/vector-icons/FontAwesome';
+
 export default class Partidas extends Component {
   state = { load: null };
 
   componentDidMount() {
     this.load();
   }
+
+  keyExtractor = (item, index) => index.toString();
+
+  renderItem = ({ item }) => (
+    <ListItem
+      title={`${item.NomePartida}  ( ${item.Esporte} )`}
+      subtitle={`Dia: ${item.Data}  -  Hora: ${item.Hora}`}
+      leftIcon={<Icon name="calendar" color="#1565C0" size={22} />}
+      style={{ marginBottom: 6 }}
+    />
+  );
 
   load = async () => {
     store.get('PARTIDAS').then(res => {
@@ -16,8 +30,18 @@ export default class Partidas extends Component {
 
   render() {
     return (
-      <View>
-        <Text>{JSON.stringify(this.state.load)}</Text>
+      <View style={{ margin: 30 }}>
+        {this.state.load === null ? (
+          <Text style={{ textAlign: 'center', fontSize: 23 }}>
+            Nenhuma partida encontrada!
+          </Text>
+        ) : (
+          <FlatList
+            keyExtractor={this.keyExtractor}
+            data={this.state.load}
+            renderItem={this.renderItem}
+          />
+        )}
       </View>
     );
   }
